0293b7c4be77140bb161906ce9d4d4d9010166cc
Lykeios Prog <135772791+LykeiosProg@users.noreply.github.com>
2023-10-22 18:26:12 +0200

    Lancement

---
+++ js/formation.js
@@ -0,0 +1,9 @@
+function afficherPlan(){
+    $('#imageModal1').modal('hide');
+    $('#imageModal1').modal('show');
+}
+
+function afficherProg(){
+    $('#imageModal2').modal('hide');
+    $('#imageModal2').modal('show');
+}
